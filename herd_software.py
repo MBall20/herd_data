fea169988319bda970b9b649923e177026a51f6f
Mitchell Ball <bal20049@byui.edu>
2022-01-14 15:54:15 -0700

    Created first table

--- herd_software.py
+++ herd_software.py
@@ -11,15 +11,48 @@ import sqlite3
 connection = sqlite3.connect('cattle_database.db')
 cursor = connection.cursor()
 
+# Create base table to work with if it does not already exist
+cursor.execute("CREATE TABLE IF NOT EXISTS herd (ear_tag INT, birth_date DATE, gender CHAR, polled CHAR, location TEXT)")
+
 # Display menu options
 choice = None
 while choice != 6:
-    print('1) Select Herd')
+    # Choice 1 is displayed so I can make minor changes when it comes time to implement it.
+
+    print('1) Select Herd (Inoperable right now)')
     print('2) Display Livestock')
     print('3) Add Livestock')
     print('4) Update Livestock')
     print('5) Delete Livestock')
     print('6) Quit')
+    choice = input('> ')
+    
+    '''
+    if choice == '1':
+        # Select desired herd
+        pass
+    '''
+
+    if choice == '2':
+        # Display contents of herd
+        cursor.execute('SELECT * FROM herd ORDER BY ear_tag ASC')
+        pass
+
+    elif choice == '3':
+        # Add animal to herd
+        pass
+
+    elif choice == '4':
+        # Update animal (location)
+        pass
+
+    elif choice == '5':
+        # Delete an animal
+        pass
+
+        
+
+
 
     
 '''    
